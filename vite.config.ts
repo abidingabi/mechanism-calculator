4982ef6afed9664f8a8f28023a84d4ebd3338fdd
Abigail <abigail@dogbuilt.net>
2023-02-02 14:13:59 -0500

    Fix formatting in a few places

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { resolve } from 'path'
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
- build: {
+  build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        motor: resolve(__dirname, 'tools/motor/index.html'),
+        main: resolve(__dirname, "index.html"),
+        motor: resolve(__dirname, "tools/motor/index.html"),
       },
     },
- }
+  },
 });
